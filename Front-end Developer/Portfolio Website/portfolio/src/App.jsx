9a2695f33b62965af3ab6ac9cdc161e102bfaaa3
faisalnightstar <abufaisal0165@gmail.com>
2024-08-13 19:59:43 +0530

    updated

--- Front-end Developer/Portfolio Website/portfolio/src/App.jsx
+++ Front-end Developer/Portfolio Website/portfolio/src/App.jsx
@@ -3,6 +3,7 @@ import FooterContainer from "./components/Footer/FooterContainer";
 import HomeContainer from "./components/Home-Body/HomeContainer";
 import "./App.css";
 import "./components/colors.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <NavbarContainer />
       <HomeContainer />
       <FooterContainer></FooterContainer>
+      <SpeedInsights />
     </>
   );
 }
